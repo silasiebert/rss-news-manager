e0809d61152c05465d4693613c3c22362c382bf1
Gabriel Naoto 2 <gabrielnaoto@yahoo.com.br>
2017-05-14 21:07:33 -0300

    #10 news found
    heres a table which loads all news in database and urls are clicable
    from jtable, unfortunately, java does not support underlined text, so I
    made it bold to make it different from the others.

--- GUI/src/br/udesc/argc/gui/view/models/NewsModel.java
+++ GUI/src/br/udesc/argc/gui/view/models/NewsModel.java
@@ -5,10 +5,73 @@
  */
 package br.udesc.argc.gui.view.models;
 
+import br.udesc.argc.persistence.dao.core.FactoryDAO;
+import br.udesc.argc.persistence.dao.core.NewsDAO;
+import br.udesc.argc.persistence.model.News;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author gabrielnaoto
  */
-public class NewsModel {
-    
+public class NewsModel extends AbstractTableModel {
+
+    private List<News> news;
+    private NewsDAO dao;
+
+    public NewsModel() {
+        dao = FactoryDAO.getPersistence().getNewsDAO();
+        news = dao.list();
+    }
+
+    @Override
+    public int getRowCount() {
+        return news.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        News s = news.get(rowIndex);
+        switch (columnIndex) {
+            case 0: {
+                return s.getTitle();
+            }
+            case 1: {
+                return s.getUrl();
+            }
+           
+        }
+        return null;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+
+            case 0: {
+                return "Title";
+            }
+            case 1: {
+                return "URL";
+            }
+
+        }
+        return null;
+    }
+
+    public News getNews(int index) {
+        return news.get(index);
+    }
+
+    public void update() {
+        news = dao.list();
+        fireTableDataChanged();
+    }
+
 }
