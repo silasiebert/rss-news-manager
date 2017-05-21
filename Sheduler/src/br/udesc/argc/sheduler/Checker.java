e3e46649a03264faea85a2cc5eb7fbb39ede4af2
Sila <sila.siebert@gmail.com>
2017-05-21 03:17:01 -0300

    #1 Adicionado o log e verificacao de horario
    Foi adicionado codigo para comparar os horarios cadastrados com o horario atual.

--- Sheduler/src/br/udesc/argc/sheduler/Checker.java
+++ Sheduler/src/br/udesc/argc/sheduler/Checker.java
@@ -7,6 +7,12 @@ package br.udesc.argc.sheduler;
 
 import br.udesc.argc.persistence.dao.core.ScheduleDAO;
 import br.udesc.argc.persistence.dao.jdbc.JDBCScheduleDAO;
+import br.udesc.argc.persistence.model.Schedule;
+import br.udesc.argc.rss.ExecuteQueryAndNotify;
+import br.udesc.argc.rss.Executer;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,22 +21,53 @@ import java.util.logging.Logger;
  * @author silajs
  */
 public class Checker implements Runnable {
-
+    
     private ScheduleDAO sDao;
-    //private Executer;
-    public Checker() {
+    private LogWriter log;
+    private Executer executer;
+    
+    public Checker() throws IOException {
         this.sDao = new JDBCScheduleDAO();
+        this.log = new LogWriter("log.txt");
+        this.executer = new ExecuteQueryAndNotify();
     }
-
+    
     @Override
+    
     public void run() {
+        SimpleDateFormat localDateFormat = new SimpleDateFormat("HH:mm:ss");
+        String time;
         try {
-            
+
             //sleep de 5 minutos
+            for (Schedule schedule : sDao.list()) {
+                
+                if (localDateFormat.format(schedule.getDate()).equals(localDateFormat.format(new Date()))) {
+                    executer.execute();
+                }
+            }
+            
             Thread.sleep(300000);
+            
+        } catch (IllegalArgumentException ex) {
+            try {
+                log.logMessage(ex.getMessage());
+            } catch (IOException ex1) {
+                System.out.println(ex1.getMessage());
+            }
+        } catch (IOException ex) {
+            try {
+                log.logMessage(ex.getMessage());
+            } catch (IOException ex1) {
+                System.out.println(ex1.getMessage());
+            }
         } catch (InterruptedException ex) {
-            Logger.getLogger(Checker.class.getName()).log(Level.SEVERE, null, ex);
+            try {
+                log.logMessage(ex.getMessage());
+            } catch (IOException ex1) {
+                System.out.println(ex1.getMessage());
+            }
         }
     }
-
+    
 }
