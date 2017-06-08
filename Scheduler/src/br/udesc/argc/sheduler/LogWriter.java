36b02dcd131177abeef2ba48f5bf71bf04e5893c
Sila <sila.siebert@gmail.com>
2017-06-07 21:58:20 -0300

    #6 Funcionalidade de log
    Implementado o metodo log message.
    Implementado o metod startLog.
    Implementado o metodo endLog.

--- Scheduler/src/br/udesc/argc/sheduler/LogWriter.java
+++ Scheduler/src/br/udesc/argc/sheduler/LogWriter.java
@@ -5,8 +5,10 @@
  */
 package br.udesc.argc.sheduler;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  *
@@ -16,12 +18,37 @@ public class LogWriter {
 
     private String path;
 
-    public LogWriter(String path) {
-        this.path = path;
+    public LogWriter() throws IOException {
+        this.path = "log.txt";
+        startLog();
     }
 
     public void logMessage(String message) throws IOException {
-        FileWriter write = new FileWriter(path, true);
-        write.append(message);
+        FileWriter filewriter = new FileWriter(path, true);
+        BufferedWriter out = new BufferedWriter(filewriter);
+        out.write(new Date().toString() + " - " + message);
+        out.newLine();
+        //close buffer writer
+        out.close();
+
+    }
+
+    public void startLog() throws IOException {
+        FileWriter filewriter = new FileWriter(path, true);
+        BufferedWriter out = new BufferedWriter(filewriter);
+        out.write(new Date().toString() + " - Log started");
+        out.newLine();
+        //close buffer writer
+        out.close();
+
+    }
+
+    public void endLog() throws IOException {
+        FileWriter filewriter = new FileWriter(path, true);
+        BufferedWriter out = new BufferedWriter(filewriter);
+        out.write(new Date().toString() + " - Log ended");
+        out.newLine();
+        //close buffer writer
+        out.close();
     }
 }
